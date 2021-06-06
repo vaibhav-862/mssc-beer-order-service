a39541312654dec3ad614fd5d83a5abae9263c57
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-06-06 20:36:17 +0530

    adding validation result listener - closes #20

--- src/main/java/guru/sfg/brewery/model/BeerOrderDto.java
+++ src/main/java/guru/sfg/brewery/model/BeerOrderDto.java
@@ -17,6 +17,8 @@
 
 package guru.sfg.brewery.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.OffsetDateTime;
@@ -28,6 +30,20 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerOrderDto {
+
+    @JsonProperty("id")
+    private UUID id = null;
+
+    @JsonProperty("version")
+    private Integer version = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonProperty("createdDate")
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape= JsonFormat.Shape.STRING)
+    @JsonProperty("lastModifiedDate")
+    private OffsetDateTime lastModifiedDate = null;
 
     private UUID customerId;
     private String customerRef;
--- src/main/java/guru/sfg/brewery/model/BeerOrderLineDto.java
+++ src/main/java/guru/sfg/brewery/model/BeerOrderLineDto.java
@@ -17,6 +17,8 @@
 
 package guru.sfg.brewery.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -28,6 +30,21 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerOrderLineDto {
+
+    @JsonProperty("id")
+    private UUID id = null;
+
+    @JsonProperty("version")
+    private Integer version = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape= JsonFormat.Shape.STRING)
+    @JsonProperty("createdDate")
+    private OffsetDateTime createdDate = null;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonProperty("lastModifiedDate")
+    private OffsetDateTime lastModifiedDate = null;
+
     private String upc;
     private String beerName;
     private String beerStyle;
